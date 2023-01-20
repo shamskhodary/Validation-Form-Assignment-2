fdaa1558893bbaec0aaf83eee73b28fdacd6212d
shamskhodary <shams.khodary@gmail.com>
2023-01-20 20:12:29 +0200

    Setting up client side and creating forms close #7

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react';
+import './App.css';
+import FormPage from './components/FormPage';
+
+const App:FC = () => {
+  return (
+    <div className="App">
+    <FormPage/>
+    </div>
+  );
+}
+
+export default App;
