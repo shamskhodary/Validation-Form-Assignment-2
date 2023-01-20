46a5f5fb1269884b5d32d14fb90b337412983355
shamskhodary <shams.khodary@gmail.com>
2023-01-20 20:40:45 +0200

    used proxy to link betweem the server and client sides so that I can make requests on client domain relates #9

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,11 @@
-import { FC } from 'react';
+import { FC} from 'react';
 import './App.css';
 import FormPage from './components/FormPage';
 
 const App:FC = () => {
+  // useEffect(() => {
+  //   axios.get('/api/v1/hello').then((res) => console.log(res))
+  // }, [])
   return (
     <div className="App">
     <FormPage/>
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
-const registerIndividual = require("../controllers/registerIndividual");
+const registerIndividual = require("../controllers/register");
 const checkType = require("../middleware/checkType");
 
 const router = require("express").Router();
-
+router.get('/hello', (req, res) => {
+  res.json({ msg: 'hiii there' })
+})
 router.post("/customer", checkType, registerIndividual);
 
 module.exports = router;
